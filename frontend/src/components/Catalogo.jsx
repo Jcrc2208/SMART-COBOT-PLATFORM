3f5476bd0bcbe1242767ee194e63c221adfc757c
marcooramoslana-dev <marcooramoslana@gmail.com>
2026-08-31 15:38:56 -0600

    cambios minimos

--- frontend/src/components/Catalogo.jsx
+++ frontend/src/components/Catalogo.jsx
@@ -35,6 +35,116 @@ const CATALOGO = [
       fondo: ['Sándalo', 'Almizcle blanco'],
     },
   },
+  {
+    id: 'CAT-0004',
+    nombre: 'Medianoche de Ámbar',
+    linea: 'Exclusiva',
+    familia: 'Oriental',
+    piramide: {
+      salida: ['Azafrán', 'Mandarina'],
+      corazon: ['Ámbar', 'Rosa de Damasco'],
+      fondo: ['Vainilla', 'Pachulí', 'Benjuí'],
+    },
+  },
+  {
+    id: 'CAT-0005',
+    nombre: 'Brisa del Mar',
+    linea: 'Clásica',
+    familia: 'Acuático',
+    piramide: {
+      salida: ['Brezos', 'Limón'],
+      corazon: ['Jazmín', 'Salvia'],
+      fondo: ['Musgo de roble', 'Ámbar gris'],
+    },
+  },
+  {
+    id: 'CAT-0006',
+    nombre: 'Café Negro',
+    linea: 'Exclusiva',
+    familia: 'Gourmand',
+    piramide: {
+      salida: ['Bergamota', 'Café'],
+      corazon: ['Cardamomo', 'Cacao'],
+      fondo: ['Vainilla', 'Sándalo', 'Almizcle'],
+    },
+  },
+  {
+    id: 'CAT-0007',
+    nombre: 'Silvestre',
+    linea: 'Clásica',
+    familia: 'Aromático',
+    piramide: {
+      salida: ['Lavanda', 'Romero', 'Menta'],
+      corazon: ['Geranio', 'Espliego'],
+      fondo: ['Musgo de roble', 'Vetiver'],
+    },
+  },
+  {
+    id: 'CAT-0008',
+    nombre: 'Rosa Velada',
+    linea: 'Firma',
+    familia: 'Floral',
+    piramide: {
+      salida: ['Lichi', 'Frutos rojos'],
+      corazon: ['Rosa', 'Peonía'],
+      fondo: ['Almizcle blanco', 'Ámbar'],
+    },
+  },
+  {
+    id: 'CAT-0009',
+    nombre: 'Tabaco y Miel',
+    linea: 'Exclusiva',
+    familia: 'Ambarado',
+    piramide: {
+      salida: ['Ciruela', 'Canela'],
+      corazon: ['Tabaco', 'Miel', 'Jazmín'],
+      fondo: ['Cuero', 'Vainilla', 'Musgo'],
+    },
+  },
+  {
+    id: 'CAT-0010',
+    nombre: 'Incienso Antiguo',
+    linea: 'Exclusiva',
+    familia: 'Oriental',
+    piramide: {
+      salida: ['Pimienta rosa', 'Benjuí'],
+      corazon: ['Incienso', 'Mirra', 'Pachulí'],
+      fondo: ['Ámbar', 'Sándalo', 'Cedro'],
+    },
+  },
+  {
+    id: 'CAT-0011',
+    nombre: 'Magnolia Blanca',
+    linea: 'Clásica',
+    familia: 'Floral',
+    piramide: {
+      salida: ['Neroli', 'Bergamota'],
+      corazon: ['Magnolia', 'Azahar'],
+      fondo: ['Sándalo', 'Almizcle'],
+    },
+  },
+  {
+    id: 'CAT-0012',
+    nombre: 'Cítrico Nocturno',
+    linea: 'Firma',
+    familia: 'Cítrico',
+    piramide: {
+      salida: ['Naranja amarga', 'Mandarina'],
+      corazon: ['Neroli', 'Jazmín'],
+      fondo: ['Vetiver', 'Ámbar'],
+    },
+  },
+  {
+    id: 'CAT-0013',
+    nombre: 'Bosque Boreal',
+    linea: 'Exclusiva',
+    familia: 'Amaderado',
+    piramide: {
+      salida: ['Enebro', 'Pino'],
+      corazon: ['Cedro', 'Abeto'],
+      fondo: ['Musgo de roble', 'Vainilla'],
+    },
+  },
 ]
 
 function CatalogoCard({ perfume }) {
